aae861175f396077970d327f0d1702809d414c55
Zach Mandeville <code@solarpunk.cool>
2023-07-19 16:44:55 +1200

    Add helper commands for ansible dir

--- flake.nix
+++ flake.nix
@@ -1,25 +1,40 @@
 {
   description = "Flake for running ansible scripts";
-  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = ["x86_64-linux" "x86_64-darwin"  "aarch64-linux" "aarch64-darwin"];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
 
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+          ];
+        };
     in
       {
-        devShells = forAllSystems(system:
-          let pkgs = nixpkgsFor.${system};
-          in
-            {
-              default = pkgs.mkShell {
-                buildInputs = with pkgs; [ ansible ];
-                shellHook = ''
-                echo "Welcome to the ansible-shell!"
-                '';
-              };
-            });
-      };
+        devShells.default =
+          pkgs.devshell.mkShell {
+            packages = [
+              pkgs.ansible
+            ];
+            commands = [
+              {
+                name = ''new-role'';
+                help = ''scaffolds new role with given name'';
+                command =
+                  ''
+                    ROLESDIR=$(git rev-parse --show-toplevel)/roles/$1
+                    mkdir -p $ROLESDIR/{tasks,templates,files,defaults,meta}
+                    touch $ROLESDIR/{tasks,templates,defaults,meta}/main.yml
+                    echo "# $1" >> $ROLESDIR/README.md
+                  '';
+              }
+            ];
+          };
+      });
 }
