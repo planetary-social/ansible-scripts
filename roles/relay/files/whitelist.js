1a7196041bad8ce0e71a74dbd69ae8919e360bb6
Daniel Cadenas <dcadenas@gmail.com>
2024-03-05 10:29:57 -0300

    Tweak to allow nip05 subdomains, refactor

--- roles/relay/files/whitelist.js
+++ roles/relay/files/whitelist.js
@@ -2,7 +2,7 @@
 
 const WHITELIST = {
   pubs: {
-    add5190be4673768546c18b565da3a699241f0e06a75e2dbc03f18663d1b7b27: true, //Reportinator
+    add5190be4673768546c18b565da3a699241f0e06a75e2dbc03f18663d1b7b27: true, // Reportinator
   },
   eventKinds: [
     0, // Metadata
@@ -11,34 +11,29 @@ const WHITELIST = {
   ],
 };
 
-const rl = require('readline').createInterface({
+const rl = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false,
 });
 
-rl.on('line', (line) => {
+rl.on("line", (line) => {
   let req = JSON.parse(line);
 
-  if (req.type === 'lookback') {
-    return; // do nothing
-  }
-
-  if (req.type !== 'new') {
-    console.error('unexpected request type'); // will appear in strfry logs
+  if (req.type === "lookback" || req.type !== "new") {
     return;
   }
 
   let res = { id: req.event.id }; // must echo the event's id
 
-  if (
-    WHITELIST.pubs[req.event.pubkey] ||
-    WHITELIST.eventKinds.includes(req.event.kind)
-  ) {
-    res.action = 'accept';
+  const isWhitelistedPub = WHITELIST.pubs.hasOwnProperty(req.event.pubkey);
+  const isWhitelistedEventKind = WHITELIST.eventKinds.includes(req.event.kind);
+
+  if (isWhitelistedPub || isWhitelistedEventKind) {
+    res.action = "accept";
   } else {
-    res.action = 'reject';
-    res.msg = 'blocked: not on white-list';
+    res.action = "reject";
+    res.msg = "blocked: pubkey not on white-list or event kind not allowed";
   }
 
   console.log(JSON.stringify(res));
